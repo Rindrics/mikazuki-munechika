af0d985f5c2f2e4f62f0477b22680ca2d0240479
Akira Hayashi <rindrics@gmail.com>
2025-12-04 08:07:20 +0900

    Add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +15,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        <nav className="pr-8 py-2 bg-gray-100 flex items-center justify-end gap-8 text-md">
+          <Link href="/">資源評価 web</Link>
+          <div className="flex items-center gap-2">
+            <Link
+              href="https://github.com/Rindrics/mikazuki-munechika/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center"
+            >
+              <FaGithub className="w-5 h-5" />
+            </Link>
+          </div>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
