fdd749952976cbbbba15493a9cb77c2c400b3ef7
Akira Hayashi <rindrics@gmail.com>
2025-12-09 05:03:08 +0900

    Remove `USER` from envvar name

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -2,3 +2,4 @@ export * from "./in-memory-assessment-result-repository";
 export * from "./in-memory-user-repository";
 export * from "./supabase-user-repository";
 export * from "./user-repository-factory";
+export * from "./assessment-result-repository-factory";
--- src/infrastructure/user-repository-factory.ts
+++ src/infrastructure/user-repository-factory.ts
@@ -5,8 +5,8 @@ import { SupabaseUserRepository } from "./supabase-user-repository";
 export function createUserRepository(): UserRepository {
   const useInMemory =
     typeof window !== "undefined"
-      ? process.env.NEXT_PUBLIC_USE_IN_MEMORY_USER_REPOSITORY === "true"
-      : process.env.USE_IN_MEMORY_USER_REPOSITORY === "true";
+      ? process.env.NEXT_PUBLIC_USE_IN_MEMORY_REPOSITORY === "true"
+      : process.env.USE_IN_MEMORY_REPOSITORY === "true";
 
   if (useInMemory) {
     return new InMemoryUserRepository();
