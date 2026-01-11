7cfd2bcb066e737f081fb17692e33d93dcaf90fb
Akira Hayashi <rindrics@gmail.com>
2026-01-11 11:57:41 +0900

    chore: redirect to rindrics.com

--- terraform/domains.tf
+++ terraform/domains.tf
@@ -11,6 +11,9 @@ import {
 resource "vercel_project_domain" "learn_to_live" {
   project_id = data.vercel_project.main.id
   domain     = "stock-assessment.learn-to.live"
+
+  redirect             = vercel_project_domain.rindrics_com.domain
+  redirect_status_code = 308
 }
 
 import {
