abcce07c5aa53d9b03355dd27ae12138bed614a0
Akira Hayashi <rindrics@gmail.com>
2026-01-11 11:58:16 +0900

    chore: remove applied import block

--- terraform/domains.tf
+++ terraform/domains.tf
@@ -3,20 +3,10 @@ resource "vercel_project_domain" "rindrics_com" {
   domain     = "stock-assessment.rindrics.com"
 }
 
-import {
-  to = vercel_project_domain.rindrics_com
-  id = "${data.vercel_project.main.id}/stock-assessment.rindrics.com"
-}
-
 resource "vercel_project_domain" "learn_to_live" {
   project_id = data.vercel_project.main.id
   domain     = "stock-assessment.learn-to.live"
 
   redirect             = vercel_project_domain.rindrics_com.domain
   redirect_status_code = 308
-}
-
-import {
-  to = vercel_project_domain.learn_to_live
-  id = "${data.vercel_project.main.id}/stock-assessment.learn-to.live"
 }
